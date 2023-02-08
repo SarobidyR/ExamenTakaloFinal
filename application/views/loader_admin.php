45b8edb48126f93e1c343eb0cc37d177c5286f5a
annahherinantenaina <fy948564@gmail.com>
2023-02-08 11:53:58 +0100

    change admin

---
+++ application/views/loader_admin.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$this->load->view('Admin_header');
+$this->load->view('Admin_content');
+$this->load->view('Admin_footer');
+?>
