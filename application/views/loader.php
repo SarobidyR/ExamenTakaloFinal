5ac035ef9772b06856d4ac7f205b0bbe91cd66c0
ramanoara-youcky <120625336+ramanoara-youcky@users.noreply.github.com>
2023-02-08 10:20:40 +0000

    login

--- application/views/loader.php
+++ application/views/loader.php
@@ -2,6 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 $this->load->view('header');
 $this->load->view('landing_User');
-$this->load->view('login_page');
+// $this->load->view('login_page');
 $this->load->view('footer');
 ?>
